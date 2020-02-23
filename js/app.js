f4ddc245738ba68fb80c506c2852286e1d7d9604
smk1025@wildcats.unh.edu <sean@LAPTOP-VJVPEPJI.localdomain>
2020-02-22 23:49:41 -0500

    final skeleton

--- js/app.js
+++ js/app.js
@@ -1,9 +1,15 @@
 "use-strict";
 
-const statistics = ["state1", "state2", "state3"];
-let nextSlide = 0;
+const STATE_TYPE = 0;
+const FORM_POS = 1;
+const STATES = 2;
+const FORM_DATA = 3;
+
+const statistics = [1, 1, ["slide0-state1.png", "slide0-state2.png", "slide0-state3.png", "slide0-state4.png"]];
+let next = 0;
 
 function nextSlide() {
-  document.getElementById("current-slide").src = statistics[nextSlide];
-  nextSlide++;
+  console.log(statistics[STATES][next]);
+  document.getElementById("current-slide").src = "assets/slides/" + statistics[STATES][next];
+  next++;
 }
