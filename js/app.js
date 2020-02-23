a63bdbbf84f8304f2726942da4ecf1f227650765
smk1025@wildcats.unh.edu <sean@LAPTOP-VJVPEPJI.localdomain>
2020-02-23 02:00:26 -0500

    finalize slideshow design

--- js/app.js
+++ js/app.js
@@ -8,11 +8,12 @@ const FORM_DATA = 3;
 const TYPE_TEXT = 1;
 const TYPE_RADIO = 2;
 
-const statistics = [2, 0, ["statistics/slide0-state1.png", "statistics/slide0-state2.png", "statistics/slide0-state3.png", "statistics/slide0-state4.png"], ["Tree", "Bird"]];
+const statistics = [2, 0, ["statistics/state1.png", "statistics/state2.png", "statistics/state3.png"], ["Tree", "Bird"]];
 const simulation = [0, 0, ["simulation/state0.png", "simulation/state1.png", "simulation/state2.png", "simulation/state3.png", "simulation/state4.png", "simulation/state5.png"], []]
 const slideshow = [statistics, simulation];
 
-let next = 0;
+let nextState = 0;
+let currentSlide = 0;
 let formPresent = 0;
 
 function nextSlide() {
@@ -30,15 +31,25 @@ function nextSlide() {
   }
   
   // Show next image on slideshow
-  document.getElementById("current-slide").src = "assets/slides/" + statistics[STATES][next];
+  document.getElementById("current-slide").src = "assets/slides/" + slideshow[currentSlide][STATES][nextState];
+
 
   // Place form on slideshow
-  if (statistics[FORM_POS] == next) {
+  if (slideshow[currentSlide][FORM_POS] == nextState && slideshow[currentSlide][FORM_TYPE]) {
     addForm(statistics[FORM_TYPE] - 1, statistics[FORM_DATA]); // 1st parameter is "-1" so that it aligns with TYPE_TEXT and TYPE_RADIO
     formPresent = 1;
   }
 
-  next++;
+  nextState++;
+
+  // Slide change
+  if (nextState == slideshow[currentSlide][STATES].length) {
+    currentSlide++;
+    nextState = 0;
+    //console.log("HERE");
+  }
+
+  console.log("nst: " + nextState + " " + "csl: " + currentSlide);
 }
 
 // type: 0 - text input  1 - radio
